91eebe2a23519a1d8876e7b3400ff3c2319f93b0
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2026-04-28 20:57:53 -0500

    Laboratorio 3 finalizado

---
+++ pokedex/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(request):
+    pokemons = ["Pikachu", "Charmander", "Bulbasaur", "Squirtle", "Eevee", "Jigglypuff", "Meowth", "Psyduck", "Snorlax", "Gengar"]
+    return render(request, 'index.html', {'pokemons': pokemons})
+
+def pokemon_details(request, pokemon):
+    return render(request, 'details.html', {'pokemon': pokemon})
